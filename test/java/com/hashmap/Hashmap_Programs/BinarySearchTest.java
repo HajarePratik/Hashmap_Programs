3fc2c29be418d3d9df4ce5b7f889df1869ac9893
HajarePratik <pratikhajare09@gmail.com>
2021-06-10 22:11:28 +0530

    Create a Binary Tree and Adding a Extra Value

--- test/java/com/hashmap/Hashmap_Programs/BinarySearchTest.java
+++ test/java/com/hashmap/Hashmap_Programs/BinarySearchTest.java
@@ -19,5 +19,26 @@ public class BinarySearchTest {
 		Assert.assertEquals(3, size);
 	}
 	
+	// UC2 create a binary tree and adding value extra value
+	@Test
+	public void given13Number_WhenAddedBSTShouldReturnSize() {
+    	MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+    	binaryTree.add(56);
+    	binaryTree.add(30);
+    	binaryTree.add(70);
+    	binaryTree.add(11);
+    	binaryTree.add(22);
+    	binaryTree.add(33);
+    	binaryTree.add(44);
+    	binaryTree.add(55);
+    	binaryTree.add(66);
+    	binaryTree.add(77);
+    	binaryTree.add(88);
+    	binaryTree.add(99);
+    	binaryTree.add(13);
+    	int size = binaryTree.getSize();
+    	Assert.assertEquals(13,size);
+	}
+	
 	
 }
